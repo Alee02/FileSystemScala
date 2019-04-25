53f40666b83b7795fbcf714281a77c9b92e8a5ef
Ali Shah <ashah@vtsl.net>
2019-04-25 09:22:59 +0100

    Basic Scaffolding state and data types setup

---
+++ src/main/scala/commands/UnknownCommand.scala
@@ -0,0 +1,6 @@
+package commands
+import filesystem.State
+
+class UnknownCommand extends Command {
+  override def apply(state: State): State = state.setMessage("Command not found!")
+}
--- src/main/scala/filesystem/FileSystem.scala
+++ src/main/scala/filesystem/FileSystem.scala
@@ -2,12 +2,18 @@ package filesystem
 
 import java.util.Scanner
 
+import commands.Command
+import files.Directory
+
 object FileSystem extends App{
 
+  val root = Directory.ROOT
+  var state = State(root, root)
   val scanner = new Scanner(System.in)
 
   while(true) {
-    println("$ ")
-    println(scanner.nextLine())
+    state.show
+    val input = scanner.nextLine()
+    state = Command.from(input).apply(state)
   }
 }
